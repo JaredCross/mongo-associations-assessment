1b7b09446e42ecdf38389e377b8576a9d57dda26
Jared Cross <cross.jared@gmail.com>
2015-08-23 20:15:15 -0600

    all

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var session = require('cookie-session')
+var session = require('cookie-session');
 var bodyParser = require('body-parser');
 
 require('dotenv').load();
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2, process.env.SESSION_KEY3] }))
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2, process.env.SESSION_KEY3] }));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var newUser = require('../lib/database/users').create;
-var login = require('../lib/database/users').login;
-var findUser = require('../lib/database/users').findUser;
-var newIngredient = require('../lib/database/users').createOrAddIngredient;
-var addRecipe = require('../lib/database/users').createRecipe;
-var lookupRecipeData = require('../lib/database/users').lookupRecipeData;
-var findAllIngredients = require('../lib/database/users').findAllUserIngredients;
-var findThisRecipeIngredients = require('../lib/database/users').findThisRecipeIngredients;
+var db = require('../lib/database');
 
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
 router.post('/user', function (req, res, next) {
-  var firstName = req.body.first_name;
-  var lastName = req.body.last_name;
-  var email = req.body.email;
-  var password = req.body.password;
-  var confirmation = req.body.confirmation;
-  newUser(email, firstName, lastName, password, confirmation).then(function (data) {
-    if (data === 'success') {
-      req.session.email = email;
-      res.redirect('/');
+  db.newUser(req.body.email, req.body.first_name, req.body.last_name, req.body.password, req.body.confirmation).then(function (data) {
+    if (data._id) {
+      req.session.id = data._id;
+      res.redirect('/user/'+data._id);
     } else {
       res.render('new-user', {errors : data});
     }
@@ -38,9 +26,9 @@ router.get('/user/login', function (req, res, next) {
 });
 
 router.post('/user/login', function (req, res, next) {
-  login(req.body.login_email, req.body.login_password).then(function (data) {
-    if (data != 'Incorrect email or password') {
-      req.session.email = req.body.login_email;
+  db.login(req.body.login_email, req.body.login_password).then(function (data) {
+    if (data._id) {
+      req.session.id = data._id;
       res.redirect('/user/' + data._id);
     } else {
       res.render('login', {error : data});
@@ -50,11 +38,15 @@ router.post('/user/login', function (req, res, next) {
 
 router.get('/user/:id', function (req, res, next) {
   var userData;
-  findUser(req.params.id).then(function (data) {
+  var recipeData;
+  db.findUser(req.params.id).then(function (data) {
     userData = data;
-    return lookupRecipeData(data.recipes);
+    return db.lookupRecipeData(data.recipes);
   }).then(function (data) {
-    res.render('user-show', {userData : userData, recipeData : data});
+    recipeData = data;
+    return db.findAllUserIngredients(req.params.id);
+  }).then(function (data) {
+    res.render('user-show', {userData : userData, recipeData : recipeData, allIngredients : data});
   });
 });
 
@@ -63,29 +55,98 @@ router.get('/user/:id/recipes/new', function (req, res, next) {
 });
 
 router.post('/user/:id/recipes', function (req, res, next) {
-  addRecipe(req.body.recipe_name, req.params.id).then(function (recipeData) {
+  db.createRecipe(req.body.recipe_name, req.params.id).then(function (recipeData) {
     res.redirect('/user/' + req.params.id + '/recipes/'+recipeData._id+'/'+recipeData.recipeName+'/ingredients/new');
+  });
+});
+
+router.get('/user/:id/ingredients/:name/:ingredientId', function (req, res, next) {
+  db.ingredientData(req.params.id, req.params.ingredientId).then(function (data) {
+    res.render('ingredient-show', {ingredient: req.params.name, recipes : data, userId : req.params.id});
+  });
+
+});
+
+router.get('/user/:id/ingredients/edit', function (req, res, next) {
+  db.findAllUserIngredients(req.params.id).then(function (data) {
+      res.render('ingredients-edit', {allIngredients : data, userData : req.params.id});
+  });
+});
+
+router.post('/user/:id/ingredients/update', function (req, res, next) {
+  db.updateIngredient(req.body.ingredientId, req.body.ingredientName).then(function () {
+    res.redirect('/user/'+req.params.id+'/ingredients/edit');
+  });
+});
+
+router.post('/user/:id/ingredients/delete', function (req, res, next) {
+  db.deleteIngredient(req.body.ingredientId).then(function () {
+    res.redirect('/user/'+req.params.id+'/ingredients/edit');
+  });
+});
+
+router.get('/user/:id/recipes/:name/:recipeId', function (req, res, next) {
+  db.findThisRecipeIngredients(req.params.recipeId).then(function (data) {
+    res.render('recipe-show', {theseIngredients : data, recipeName : req.params.name, userId : req.params.id, recipeId : req.params.recipeId});
+  });
+});
+
+
+router.get('/user/:id/recipes/:name/:recipeId/edit', function (req, res, next) {
+  var allIngredients;
+  db.findAllUserIngredients(req.params.id).then(function (data) {
+    allIngredients = data;
+    return db.findThisRecipeIngredients(req.params.recipeId);
+  }).then(function (data) {
+    res.render('recipe-edit', {userData : req.params.id, recipeName: req.params.name, recipeId: req.params.recipeId, theseIngredients: data, allIngredients: allIngredients});
+  });
+});
+
+router.post('/user/:id/recipes/:name/:recipeId', function (req, res, next) {
+  db.updateRecipeName(req.params.recipeId, req.body.recipe_name).then(function (data) {
+    res.redirect('/user/' +req.params.id+'/recipes/'+req.body.recipe_name+'/'+req.params.recipeId+'/edit');
+  });
+});
+
+router.post('/user/:id/recipes/:name/:recipeId/delete', function (req, res, next) {
+  db.deleteRecipe(req.params.recipeId, req.params.id).then(function () {
+    res.redirect('/user/'+req.params.id);
   });
 });
 
 router.get('/user/:id/recipes/:recipeId/:name/ingredients/new', function (req, res, next) {
   var allRecipeIngredients;
-  findAllIngredients(req.params.id).then(function (allRecipeIngredientsData) {
+  db.findAllUserIngredients(req.params.id).then(function (allRecipeIngredientsData) {
     allRecipeIngredients = allRecipeIngredientsData;
-    return findThisRecipeIngredients(req.params.recipeId);
+    return db.findThisRecipeIngredients(req.params.recipeId);
   }).then(function (data) {
     res.render('ingredients-new', {recipeName : req.params.name, userId : req.params.id, recipeId : req.params.recipeId, allIngredients : allRecipeIngredients, theseIngredients : data});
   });
+});
 
 
+router.post('/user/:id/recipes/:name/:recipeId/ingredients/new/update', function (req, res, next) {
+  db.newOrAddIngredient(req.body.add_ingredient, req.body.choose_ingredient, req.params.recipeId).then(function (data) {
+    res.redirect('/user/'+req.params.id+'/recipes/'+req.params.name+'/'+req.params.recipeId+'/edit');
+  });
 });
 
 router.post('/user/:id/recipes/:recipeId/:name/ingredients', function (req, res, next) {
-  newIngredient(req.body.add_ingredient, req.body.choose_ingredient, req.params.recipeId).then(function (data) {
+  db.newOrAddIngredient(req.body.add_ingredient, req.body.choose_ingredient, req.params.recipeId).then(function (data) {
     res.redirect('/user/'+req.params.id+'/recipes/'+req.params.recipeId+'/'+req.params.name+'/ingredients/new');
   });
 });
 
+router.post('/user/:id/recipes/:name/:recipeId/ingredients/update', function (req, res, next) {
+  db.updateIngredient(req.body.ingredientId, req.body.ingredientName).then(function () {
+    res.redirect('/user/'+req.params.id+'/recipes/'+req.params.name+'/'+req.params.recipeId+'/edit');
+  });
+});
 
+router.post('/user/:id/recipes/:name/:recipeId/ingredients/delete', function (req, res, next) {
+  db.deleteIngredient(req.body.ingredientId, req.params.recipeId).then(function () {
+    res.redirect('/user/'+req.params.id+'/recipes/'+req.params.name+'/'+req.params.recipeId+'/edit');
+  });
+});
 
 module.exports = router;
